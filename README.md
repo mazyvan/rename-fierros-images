06aa88e4a1c13c2620c1ff708f2fca1bc82d8041
Ivan Sanchez <mazyvan@hotmail.com>
2018-09-05 13:37:20 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Fierros Rename Files - Bash script
+
+Simple script que renombra los archivos dentro de una carpta con el nombre de la carpeta abuelo
+
+### Uso
+Modificar la variable `images_path` con la ruta correcta y ejecutar el script desde la terminal
---
+++ rename-fierros-files-script.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Fierros Rename Files - Bash script, by Iván Sánchez
+set -e
+
+echo 'Fierros Rename Files - Bash script, by Iván Sánchez'
+echo ""
+echo "[Warning]: Do not perform any operations on the directory while this script is running"
+read -n1 -r -p "Press any key to continue... " key
+echo ""
+
+images_path="./images"
+
+for dir in $images_path/*/; do
+  SAVEIFS=$IFS
+  IFS=$(echo -en "\n\b")
+
+  dirname=$(basename "$dir")
+  echo '- Moving to folder "'$dir'"'
+
+  # Trying to get files inside "Fierro" folder
+  fierro_folder=`ls "${dir}Fierro"`
+  for file in $fierro_folder; do
+    file_extension="${file##*.}"
+    # Renaming the 'fierro' image
+    old_file="${dir}Fierro/$file"
+    new_file="${dir}Fierro/${dirname}.${file_extension}"
+    if [ "${old_file}" != "${new_file}" ]
+    then
+      mv "$old_file" "$new_file"
+      echo '  - File "'$old_file'" successfully renamed to "'$new_file'"'
+    fi
+  done
+  # Trying to get files inside "Foto" folder
+  foto_folder=`ls "${dir}Foto"`
+  for file in $foto_folder; do
+    file_extension="${file##*.}"
+    # Renaming the 'foto' image
+    old_file="${dir}Foto/${file}"
+    new_file="${dir}Foto/${dirname}.${file_extension}"
+    if [ "${old_file}" != "${new_file}" ]
+    then
+      mv "${old_file}" "${new_file}"
+      echo '  - File "'$old_file'" successfully renamed to "'$new_file'"'
+    fi
+  done
+  IFS=$SAVEIFS
+done
+
+echo ""
+echo "Every file in the path has been successfully renamed"
