05a9b97b97411a183eae929312fa65e2cee95470
Ivan Sanchez <mazyvan@hotmail.com>
2018-09-05 13:52:06 -0500

    moving ifs at top and restoring at bottom of the file

--- rename-fierros-files-script.sh
+++ rename-fierros-files-script.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Fierros Rename Files - Bash script, by Iván Sánchez
+
 set -e
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
 
 echo 'Fierros Rename Files - Bash script, by Iván Sánchez'
 echo ""
@@ -11,8 +14,6 @@ echo ""
 images_path="./images"
 
 for dir in $images_path/*/; do
-  SAVEIFS=$IFS
-  IFS=$(echo -en "\n\b")
 
   dirname=$(basename "$dir")
   echo '- Moving to folder "'$dir'"'
@@ -30,6 +31,7 @@ for dir in $images_path/*/; do
       echo '  - File "'$old_file'" successfully renamed to "'$new_file'"'
     fi
   done
+
   # Trying to get files inside "Foto" folder
   foto_folder=`ls "${dir}Foto"`
   for file in $foto_folder; do
@@ -43,8 +45,10 @@ for dir in $images_path/*/; do
       echo '  - File "'$old_file'" successfully renamed to "'$new_file'"'
     fi
   done
-  IFS=$SAVEIFS
+
 done
+
+IFS=$SAVEIFS
 
 echo ""
 echo "Every file in the path has been successfully renamed"
